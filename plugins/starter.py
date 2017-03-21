3ee18c978f2f216b4fcf19c7b03a855b5f979108
anthony-handwerker <antrog.werker@yahoo.com>
2017-03-21 15:48:46 -0400

    maybe eternal support?

--- plugins/starter.py
+++ plugins/starter.py
@@ -55,7 +55,7 @@ def process_message(data):
         tokens = s1.split('{{')
 
         counter = 0
-        print("ready to find eternal cards")
+        outputs.append([data['channel'], "ready to find eternal cards"])
 
         for i in range(1, len(tokens)):
             if tokens[i].find("}}") > -1:
@@ -63,9 +63,9 @@ def process_message(data):
                 card_name = card_names[0]
                 url = {'search' : card_name}
                 temp = ul.urlencode(url)
-                print("about to open the url")
+                outputs.append([data['channel'],"about to open the url"])
                 response = ul.urlopen("www.numotgaming.com/cards/?"+temp)
-                print("url opened")
+                outputs.append([data['channel'],"url opened"])
 
                 html = str(response.read())
 
